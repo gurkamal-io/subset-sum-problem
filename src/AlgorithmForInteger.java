9f60c58b802830cc7e0619440e7e1abfab082abe
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-04-16 01:34:32 -0700

    Renamed variable names to make the for loops over specific indices more clear

--- src/AlgorithmForInteger.java
+++ src/AlgorithmForInteger.java
@@ -7,7 +7,8 @@ public class AlgorithmForInteger
       final int TARGET = 72;
       ArrayList<Integer> dataSet = new ArrayList<Integer>();
       ArrayList<SublistForInteger> choices = new ArrayList<SublistForInteger>();
-      int j, k, maxSum, choiceCount, testSum, bestSum, bestSublistIndex;
+      int dataIndex, choiceIndex, bestChoiceIndex;
+      int maxSum, newSum, bestSum, choiceCount;
       boolean foundPerfect;
 
       dataSet.add(2); dataSet.add(12); dataSet.add(22);
@@ -18,8 +19,8 @@ public class AlgorithmForInteger
 
       // Checking Algorithm Applicability
       maxSum = 0;
-      for (j = 0; j < dataSet.size(); j++)
-         maxSum += dataSet.get(j);
+      for (dataIndex = 0; dataIndex < dataSet.size(); dataIndex++)
+         maxSum += dataSet.get(dataIndex);
       if (maxSum < TARGET)
       {
          System.out.println("The target " + TARGET + " is too large.");
@@ -29,23 +30,23 @@ public class AlgorithmForInteger
       // Algorithm for Subset Sum Problem of Positive Integers
       choices.add(new SublistForInteger(dataSet));
       bestSum = 0;
-      bestSublistIndex = 0;
+      bestChoiceIndex = 0;
       foundPerfect = false;
-      for (j = 0; (j < dataSet.size()); j++)
+      for (dataIndex = 0; (dataIndex < dataSet.size()); dataIndex++)
       {
          choiceCount = choices.size();
-         for (k = 0; (k < choiceCount); k++)
+         for (choiceIndex = 0; (choiceIndex < choiceCount); choiceIndex++)
          {
-            testSum = choices.get(k).getSum() + dataSet.get(j);
-            if (testSum <= TARGET)
+            newSum = choices.get(choiceIndex).getSum() + dataSet.get(dataIndex);
+            if (newSum <= TARGET)
             {
-               choices.add(choices.get(k).addItem(j));
-               if (testSum > bestSum)
+               choices.add(choices.get(choiceIndex).addItem(dataIndex));
+               if (newSum > bestSum)
                {
-                  bestSum = testSum;
-                  bestSublistIndex = choices.size() - 1;
+                  bestSum = newSum;
+                  bestChoiceIndex = choices.size() - 1;
                }
-               if (testSum == TARGET)
+               if (newSum == TARGET)
                {
                   foundPerfect = true;
                   break;
@@ -55,6 +56,6 @@ public class AlgorithmForInteger
          if (foundPerfect)
             break;
       }
-      choices.get(bestSublistIndex).showSublist();
+      choices.get(bestChoiceIndex).showSublist();
    }
 }
